9bfc7abfe97737d609543f4ab5e4735a19f7729c
Eric Yon <eyon@Erics-MBP.lan>
2023-01-11 20:08:47 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/logo.svg";
+import mobilePicture from "./images/image-web-3-mobile.jpg";
+import desktopPicture from "./images/image-web-3-desktop.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <header>
+        <img id="header-logo" src={logo} alt="logo" />
+        {/* hamburger-menu */}
       </header>
-    </div>
+      <div className="body">
+        <div className="article">
+          <img src="mobilePicture" alt="" />
+          <span>The Bright Future of Web 3.0?</span>
+          <p>
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <button>READ MORE</button>
+        </div>
+        <div className="new">
+          {/* title */}
+          <div className="new-articles">{/* for each articles */}</div>
+        </div>
+        <div className="top-three">{/* loop component for article */}</div>
+      </div>
+    </main>
   );
 }
 
